ef75e0a6317b4d11b297c20cbab3fb3d32b9b46c
Vignesh Vangala <110151104+Vignesh960@users.noreply.github.com>
2023-02-15 23:14:41 +0530

    added colors

--- progres_bar.py
+++ progres_bar.py
@@ -1,7 +1,9 @@
 from tkinter import *
+
 root=Tk()
 root.title("ProgressBar")
 root.geometry("200x200+100+50")
+root.configure(bg="#2F6C60")
 root.resizable(0,0)
 per=StringVar()
 count=StringVar()
@@ -15,15 +17,15 @@ def start():
         bar['value']+=10
         x+=1
         per.set(str((x/tasks)*100)+"%")
-        count.set(str(x)+"/"+str(tasks)+"Completed")
+        count.set(str(x)+"/"+str(tasks)+" Completed")
         bar.update()
 
 bar=ttk.Progressbar(root,length=300,orient=HORIZONTAL)
 bar.pack(padx=10)
 '''for updating percent of totals'''
-per_label=Label(root,textvariable=per).pack()
+per_label=Label(root,textvariable=per,bg="#2F6C60",fg="#FFFFFF").pack()
 '''for updating count of totals'''
-count_label=Label(root,textvariable=count).pack()
+count_label=Label(root,textvariable=count,width=20,background="#2F6C60",fg="#FFFFFF").pack()
 
-Button(root,text="Download",command=start).pack(padx=10)
+Button(root,text="Download",command=start).pack(padx=10,pady=10)
 root.mainloop()
