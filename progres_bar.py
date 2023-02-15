24520218d841f36f64eecc4fb6de6b77c77f2521
Vignesh Vangala <110151104+Vignesh960@users.noreply.github.com>
2023-02-15 23:05:16 +0530

    progress bar

---
+++ progres_bar.py
@@ -0,0 +1,29 @@
+from tkinter import *
+root=Tk()
+root.title("ProgressBar")
+root.geometry("200x200+100+50")
+root.resizable(0,0)
+per=StringVar()
+count=StringVar()
+def start():
+    
+    tasks=10
+    x=0
+    bar['value']=0
+    while x<tasks:
+        time.sleep(0.5)
+        bar['value']+=10
+        x+=1
+        per.set(str((x/tasks)*100)+"%")
+        count.set(str(x)+"/"+str(tasks)+"Completed")
+        bar.update()
+
+bar=ttk.Progressbar(root,length=300,orient=HORIZONTAL)
+bar.pack(padx=10)
+'''for updating percent of totals'''
+per_label=Label(root,textvariable=per).pack()
+'''for updating count of totals'''
+count_label=Label(root,textvariable=count).pack()
+
+Button(root,text="Download",command=start).pack(padx=10)
+root.mainloop()
